6783441d1de351fa378ddff065c415b21f546078
Geelder <90773429+Geelder@users.noreply.github.com>
2023-11-27 23:11:39 +0300

    Update Телега бот.py ver. 1.1

--- Телега бот.py
+++ Телега бот.py
@@ -10,7 +10,7 @@ current_time = datetime.datetime.now(moscow_timezone).time()
 # Проверка, что текущее время 8:00 утра
 if current_time.hour == 8 and current_time.minute == 0:
     # Получение данных о погоде в Минске через OpenWeatherMap API
-    weather_api_key = '36603adea91a48c7214a124c34e64a7c'  # Замените на ваш ключ API для OpenWeatherMap
+    weather_api_key = 'a0c6d6c8243c4b7ad926cb76f34d7640'  # Замените на ваш ключ API для OpenWeatherMap
     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid={weather_api_key}&units=metric'
     response = requests.get(weather_url)
     weather_data = response.json()
@@ -21,6 +21,6 @@ if current_time.hour == 8 and current_time.minute == 0:
     message = f'Доброе утро Бубля! В Минске сейчас {temperature}°C, {weather_description}.'
 
     # Отправка сообщения в телеграм
-    bot = telebot.TeleBot('6910536186:AAEJA39qoi9hP6IWdEq7kDQloK2RxsfimWQ')  # Замените на ваш токен бота
+    bot = telebot.TeleBot('6910536186:AAEJA39qoi9hP6IWdEq7kDQloK2RxsfimWQ')  # Замените на ваш токен бота. Я - 449868007, Бубля - 928848734. 
     chat_id = 'YOUR_CHAT_ID'  # Замените на ваш chat_id
     bot.send_message(chat_id, message)
