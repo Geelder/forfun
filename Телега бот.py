6bf577fe81413533a372f2e6f7a0937168747a91
Geelder <90773429+Geelder@users.noreply.github.com>
2023-11-27 20:59:30 +0300

    Add files via upload

---
+++ Телега бот.py
@@ -0,0 +1,26 @@
+import telebot
+import requests
+import datetime
+import pytz
+
+# Получение текущего времени в Москве
+moscow_timezone = pytz.timezone('Europe/Moscow')
+current_time = datetime.datetime.now(moscow_timezone).time()
+
+# Проверка, что текущее время 8:00 утра
+if current_time.hour == 8 and current_time.minute == 0:
+    # Получение данных о погоде в Минске через OpenWeatherMap API
+    weather_api_key = '36603adea91a48c7214a124c34e64a7c'  # Замените на ваш ключ API для OpenWeatherMap
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid={weather_api_key}&units=metric'
+    response = requests.get(weather_url)
+    weather_data = response.json()
+
+    # Формирование сообщения с погодой и пожеланием
+    temperature = weather_data['main']['temp']
+    weather_description = weather_data['weather'][0]['description']
+    message = f'Доброе утро Бубля! В Минске сейчас {temperature}°C, {weather_description}.'
+
+    # Отправка сообщения в телеграм
+    bot = telebot.TeleBot('6910536186:AAEJA39qoi9hP6IWdEq7kDQloK2RxsfimWQ')  # Замените на ваш токен бота
+    chat_id = 'YOUR_CHAT_ID'  # Замените на ваш chat_id
+    bot.send_message(chat_id, message)
